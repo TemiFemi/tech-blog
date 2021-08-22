4763a4c0fef51034272b996ee2898f6bb0fe9d50
Temitayo Jimoh <temi.boy@MacBook-Pro-3.local>
2021-08-21 23:57:50 -0700

    Added updates

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { Router } = require('express');
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
@@ -7,7 +6,7 @@ const withAuth = require('../utils/auth');
 router.get('/', withAuth, (req, res) => {
     Post.findAll({
         where: {
-            //use the ID from the session
+            // use the ID from the session
             user_id: req.session.user_id
         },
         attributes: [
@@ -32,7 +31,7 @@ router.get('/', withAuth, (req, res) => {
         ]
     })
         .then(dbPostData => {
-            // serialize data beofre passing to template
+            // serialize data before passing to template
             const posts = dbPostData.map(post => post.get({ plain: true }));
             res.render('dashboard', { posts, loggedIn: true });
         })
